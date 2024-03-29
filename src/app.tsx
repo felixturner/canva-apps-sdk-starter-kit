c50278896824d8668f266e1f36679b4eb3862e7f
felix turner <felixturner@gmail.com>
2024-03-29 15:39:46 -0700

    use overlay example

--- src/app.tsx
+++ src/app.tsx
@@ -1,27 +1,23 @@
-import { Button, Rows, Text } from "@canva/app-ui-kit";
-import { addNativeElement } from "@canva/design";
 import * as React from "react";
-import styles from "styles/components.css";
+import { appProcess } from "@canva/preview/platform";
+import { ObjectPanel } from "./object_panel";
+import { Overlay } from "./overlay";
+
+export type LaunchParams = {
+  selectedImageUrl: string;
+  brushSize: number;
+};
 
 export const App = () => {
-  const onClick = () => {
-    addNativeElement({
-      type: "TEXT",
-      children: ["Hello world!"],
-    });
-  };
+  const context = appProcess.current.getInfo<LaunchParams>();
 
-  return (
-    <div className={styles.scrollContainer}>
-      <Rows spacing="2u">
-        <Text>
-          To make changes to this app, edit the <code>src/app.tsx</code> file,
-          then close and reopen the app in the editor to preview the changes.
-        </Text>
-        <Button variant="primary" onClick={onClick} stretch>
-          Do something cool
-        </Button>
-      </Rows>
-    </div>
-  );
+  if (context.surface === "object_panel") {
+    return <ObjectPanel />;
+  }
+
+  if (context.surface === "selected_image_overlay") {
+    return <Overlay context={context} />;
+  }
+
+  throw new Error(`Invalid surface: ${context.surface}`);
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import { AppUiProvider } from "@canva/app-ui-kit";
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app";
 import "@canva/app-ui-kit/styles.css";
+import { AppUiProvider } from "@canva/app-ui-kit";
 
 const root = createRoot(document.getElementById("root")!);
 function render() {
